87d6649b21864331bf59b54aee5f62f792981ec7
atlaskua <zylyn89@gmail.com>
2023-04-05 09:50:08 +0800

    initiated

---
+++ src/main/java/ibf2022/paf/assessment/server/controllers/TestController.java
@@ -0,0 +1,5 @@
+package ibf2022.paf.assessment.server.controllers;
+
+public class TestController {
+    
+}
---
+++ src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
@@ -0,0 +1,25 @@
+package ibf2022.paf.assessment.server.repositories;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import ibf2022.paf.assessment.server.models.User;
+
+// TODO: Task 3
+@Repository
+public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public Optional<User> findUserByUsername(String username){
+
+
+        return null;
+    }
+
+
+}
