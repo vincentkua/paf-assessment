9d8dbb5057f9bb45cb2143093e8717282dcbedb3
atlaskua <zylyn89@gmail.com>
2023-04-05 11:14:56 +0800

    completed until task 5

--- src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
+++ src/main/java/ibf2022/paf/assessment/server/controllers/TasksController.java
@@ -1,8 +1,57 @@
 package ibf2022.paf.assessment.server.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.hibernate.query.ReturnableType;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ibf2022.paf.assessment.server.models.Task;
+
 // TODO: Task 4, Task 8
 
+@RestController
 public class TasksController {
 
-    
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @PostMapping(value = "task", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> addTask(@RequestBody MultiValueMap<String, String> form) throws ParseException {
+
+        String username = form.getFirst("username");
+
+        List<Task> tasks = new LinkedList<>();
+
+        // Loop through all the form parameters and add tasks to the tasks list
+        for (String key : form.keySet()) {
+            if (key.startsWith("description-")) {
+                int index = Integer.parseInt(key.split("-")[1]);
+                String description = form.getFirst(key);
+                String priority = form.getFirst("priority-" + index);
+                String dueDate = form.getFirst("dueDate-" + index);
+
+                Task task = new Task();
+                task.setDescription(description);
+                task.setPriority(Integer.parseInt(priority) );
+                task.setDueDate(dateFormat.parse(dueDate));
+
+                tasks.add(task);
+            }
+        }
+
+        System.out.println(username);
+        System.out.println(tasks);
+
+
+        return null;
+    }
+
 }
--- src/main/java/ibf2022/paf/assessment/server/models/Task.java
+++ src/main/java/ibf2022/paf/assessment/server/models/Task.java
@@ -1,6 +1,55 @@
 package ibf2022.paf.assessment.server.models;
 
+import java.util.Date;
+
 // TODO: Task 4
 
 public class Task {
+
+    private Integer taskId;
+    private String description;
+    private Integer priority;
+    private Date dueDate;
+    public Task() {
+    }
+    public Task(Integer taskId, String description, Integer priority, Date dueDate) {
+        this.taskId = taskId;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+    public Integer getTaskId() {
+        return taskId;
+    }
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getPriority() {
+        return priority;
+    }
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+    public Date getDueDate() {
+        return dueDate;
+    }
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    @Override
+    public String toString() {
+        return "Task [taskId=" + taskId + ", description=" + description + ", priority=" + priority + ", dueDate="
+                + dueDate + "]";
+    }
+    
+    
+    
+
+    
 }
