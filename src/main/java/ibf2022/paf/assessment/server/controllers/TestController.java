5fd079c659c6b2e42438da463593fd6a354c1de9
atlaskua <zylyn89@gmail.com>
2023-04-05 10:24:42 +0800

    completed task 1,2,3

--- src/main/java/ibf2022/paf/assessment/server/controllers/TestController.java
+++ src/main/java/ibf2022/paf/assessment/server/controllers/TestController.java
@@ -1,5 +1,51 @@
 package ibf2022.paf.assessment.server.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ibf2022.paf.assessment.server.models.User;
+import ibf2022.paf.assessment.server.repositories.UserRepository;
+
+@RestController
 public class TestController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/find")
+    public void findUser(){
+
+        Optional<User> userOptional = userRepository.findUserByUsername("fred6");
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            System.out.println(user);
+        } else {
+            System.out.println("user not found");
+        }
+
+    }
+
+    @GetMapping("/insert")
+    public void insertUser(){
+
+        User user = new User();
+        user.setName("Ali baba");
+        user.setUsername("Ali ");
+
+        String insertedID = userRepository.insertUser(user);
+
+        if(insertedID != null){
+            System.out.println(insertedID);
+        }else{
+            System.out.println("Unable to insert");
+        }
+        
+
+    }
+
     
 }
--- src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
+++ src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
@@ -1,8 +1,10 @@
 package ibf2022.paf.assessment.server.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -15,11 +17,46 @@ public class UserRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public Optional<User> findUserByUsername(String username){
+    private static final String FINDUSERSQL = """
+            select * from user where username = ?
+            """;
 
+    private static final String INSERTUSERSQL = """
+            insert into user
+            (user_id, username , name)
+            values
+            (? , ? , ? )
+            """;
 
-        return null;
+    public Optional<User> findUserByUsername(String username) {
+
+        try {
+            User user = jdbcTemplate.queryForObject(FINDUSERSQL, BeanPropertyRowMapper.newInstance(User.class),
+                    username);
+            return Optional.of(user);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+
     }
 
+    public String insertUser(User user) {
+
+        String newuser_id = UUID.randomUUID().toString().substring(0, 8);
+
+        try {
+            Integer rowsupdated = jdbcTemplate.update(INSERTUSERSQL, newuser_id, user.getUsername(), user.getName());
+
+            if (rowsupdated > 0) {
+                return newuser_id;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println("SQL Error");
+            return null;
+        }
+
+    }
 
 }
